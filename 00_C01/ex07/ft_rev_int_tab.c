7247d6ccfc4c5be5b817c7b3f1b4b823b844e7ac
rmoura-r <rmoura-r@student.42.rio>
2022-03-30 05:04:58 +0200

    C01 done

--- 00_C01/ex07/ft_rev_int_tab.c
+++ 00_C01/ex07/ft_rev_int_tab.c
@@ -1,16 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmoura-r <rmoura-r@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/29 00:57:11 by rmoura-r          #+#    #+#             */
-/*   Updated: 2022/03/29 00:57:11 by rmoura-r         ###   ########.fr       */
+/*   Created: 2022/03/30 02:33:54 by rmoura-r          #+#    #+#             */
+/*   Updated: 2022/03/30 03:01:43 by rmoura-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ft(int *nbr)
+void	ft_rev_int_tab(int *tab, int size)
 {
-	*nbr = 42;
+	int	i;
+	int	first;
+	int	half;
+	int	last;
+
+	i = 0;
+	half = size / 2;
+	while (i < half)
+	{
+		last = size - 1;
+		first = tab[i];
+		tab[i] = tab[last];
+		tab[last] = first;
+		i++;
+		size--;
+	}
 }
--- 00_C01/ex08/ft_sort_int_tab.c
+++ 00_C01/ex08/ft_sort_int_tab.c
@@ -1,16 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmoura-r <rmoura-r@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/29 00:57:11 by rmoura-r          #+#    #+#             */
-/*   Updated: 2022/03/29 00:57:11 by rmoura-r         ###   ########.fr       */
+/*   Created: 2022/03/30 02:33:54 by rmoura-r          #+#    #+#             */
+/*   Updated: 2022/03/30 04:50:10 by rmoura-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ft(int *nbr)
+void	ft_sort_int_tab(int *tab, int size)
 {
-	*nbr = 42;
+	int	steps;
+	int	tmp;
+	int	i;
+
+	steps = size;
+	while (steps > 0)
+	{
+		i = -1;
+		while (++i < steps -1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				tmp = tab[i + 1];
+				tab[i + 1] = tab[i];
+				tab[i] = tmp;
+			}
+		}
+		steps--;
+	}
 }
