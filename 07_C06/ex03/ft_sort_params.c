91792f5c894828ac23bed491ac27e3e214f683f1
rmoura-r <rmoura-r@student.42.rio>
2022-04-13 07:23:18 +0200

    c06 done

--- 07_C06/ex03/ft_sort_params.c
+++ 07_C06/ex03/ft_sort_params.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmoura-r <rmoura-r@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 23:41:06 by rmoura-r          #+#    #+#             */
-/*   Updated: 2022/04/13 02:38:31 by rmoura-r         ###   ########.fr       */
+/*   Updated: 2022/04/13 03:13:02 by rmoura-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,42 @@ void	ft_putstr(char *str)
 		write(1, str++, 1);
 }
 
+int	ft_strcmp(char *s1, char *s2)
+{
+	while ((*s1 && *s2) && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+}
+
 int	main(int argc, char **argv)
 {
-	while (--argc > 0)
+	int	n;
+	int	i;
+
+	n = argc;
+	while (--n > 0)
+	{	
+		i = 0;
+		while (++i < n)
+			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+				ft_swap(&argv[i], &argv[i + 1]);
+	}
+	i = 0;
+	while (++i < argc)
 	{
-		ft_putstr(argv[argc]);
+		ft_putstr(argv[i]);
 		ft_putstr("\n");
 	}
 }
