3b9e1a638814e7852dc4c2417494256ffad3b5be
rmoura-r <rmoura-r@student.42.rio>
2022-04-15 07:26:25 +0200

    ex02 done

---
+++ 08_C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmoura-r <rmoura-r@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/15 02:45:31 by rmoura-r          #+#    #+#             */
+/*   Updated: 2022/04/15 02:45:34 by rmoura-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	i;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	*range = malloc(sizeof(int) * (max - min));
+	if (!*range)
+		return (-1);
+	i = 0;
+	while (min < max)
+		*range[i++] = min++;
+	return (i);
+}
