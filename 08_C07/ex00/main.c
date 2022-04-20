d6339a092df8a026d41cb4589c403ec6c91cf247
rmoura-r <rmoura-r@student.42.rio>
2022-04-20 23:44:46 +0200

    started C09

--- 08_C07/ex00/main.c
+++ 08_C07/ex00/main.c
@@ -4,16 +4,20 @@ char	*ft_strdup(char *src);
 
 int	main(void)
 {
-	char text[] = "txt";
+	char text[] = "qwertyuioppoiuytrewq7897454651321";
 	char *res;
 
 	// char *dup;
 	// *text = ;
-	// printf("   String: %s\n", text);
-	// printf("   strdup: %s\n", strdup(text));
-	// printf("ft_strdup: %s\n", ft_strdup(text));
+	printf("   String: %s\n", text);
+	printf("   String: %p\n\n", text);
 	res = ft_strdup(text);
-	printf("res:  %s\n", res);
-	printf("resp: %p\n", &res);
+
+	printf("ft_strdup:  %s\n", res);
+	printf("resp: %p\n", res);
+
+	// printf("   strdup: %s\n", strdup(text));
+	// printf("ft_strdup: %s\n\n", ft_strdup(text));
+
 	return (0);
 }
